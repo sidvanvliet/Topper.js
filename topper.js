fb445fd85245923c48b650f6ec61159caeb4ed68
Sid van Vliet <40121703+sidvanvliet@users.noreply.github.com>
2019-01-30 21:42:05 +0100

    Initial commit

---
+++ topper.js
@@ -0,0 +1,15 @@
+/*
+Topper.js - release 1.0
+jQuery Notification Library
+https://github.com/sidvanvliet/Topper.js
+*/
+
+if(!window.jQuery)
+{
+    console.error('Topper.js requires the jQuery library');
+}
+
+function Topper(...args)
+{
+    const title = args[0].title;
+}
